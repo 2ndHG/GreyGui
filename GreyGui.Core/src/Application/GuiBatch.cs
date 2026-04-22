96ba5fc4c89cc31084ce8399f448514d64f44bc8
2ndHG <71225645+2ndHG@users.noreply.github.com>
2026-04-22 21:20:00 +0800

    Update GuiBatch

--- GreyGui.Core/src/Application/GuiBatch.cs
+++ GreyGui.Core/src/Application/GuiBatch.cs
@@ -10,7 +10,6 @@ public class GuiBatch
     private readonly GraphicsDevice _device;
     private readonly Effect _uiShader;
     private static readonly RasterizerState ScissorState = new() { ScissorTestEnable = true };
-    private GameTime _gameTime;
 
 
     private RenderContext _renderContext;
@@ -35,7 +34,6 @@ public class GuiBatch
     {
         _device = device;
         _uiShader = GreyGuiCore.Shader;
-        _gameTime = new GameTime();
 
         _renderContext = new RenderContext();
     }
@@ -47,8 +45,7 @@ public class GuiBatch
     /// <param name="gameTime">GameTime instance, typically from Game.Draw</param>
     public void ReceiveFrameInfo(GameTime gameTime)
     {
-        _gameTime = gameTime;
-        _renderContext.ElapsedTimeSecond = _gameTime.ElapsedGameTime.TotalSeconds;
+        _renderContext.ElapsedTimeSecond = gameTime.ElapsedGameTime.TotalSeconds;
     }
 
     /// <summary>
@@ -58,77 +55,6 @@ public class GuiBatch
     /// Like SpriteBatch.Draw, this method does NOT immediately draw to the current render target, instead, it add draw commands into the RenderContext.<br/> Use GuiBatch.Flush() to actually render the pixels to the render target.
     /// </remarks>
     /// <param name="root">The GreyGuiElement and its children you want to draw</param>
-    /// <param name="context">Drawing RenderContext</param>
-    /// <param name="position">Drawing position</param>
-    public void Draw(GreyGuiElement root, RenderContext context, Point position)
-    {
-        context.ElapsedTimeSecond = _gameTime.ElapsedGameTime.TotalSeconds;
-        Rectangle screenScissor = _device.Viewport.Bounds;
-        root.ResolveSizeDirty();
-        root.Draw(position, context, screenScissor);
-        if (root is IContainer container)
-        {
-            container.DrawChildren(position, context, screenScissor);
-        }
-    }
-
-    /// <summary>
-    /// Consume accumulated draw commands of a RenderContext and render to the screen 
-    /// </summary>
-    /// <param name="context"></param>
-    public void Flush(RenderContext context)
-    {
-        // Check if there are generated SDF bitmaps that haven't be drawn to the atlas
-        GreyGuiCore.TextSystem.SetGeneratedSdfBitmapToAtlas();
-
-        if (context.Batches.Count == 0)
-        {
-            return;
-        }
-
-        GreyGuiCore.EnsureGpuBufferCapacity(context.VertexCount, context.IndexCount);
-        GreyGuiCore.VertexBuffer.SetData(context.Vertices, 0, context.VertexCount, SetDataOptions.Discard);
-        GreyGuiCore.IndexBuffer.SetData(context.Indices, 0, context.IndexCount, SetDataOptions.Discard);
-        _device.SetVertexBuffer(GreyGuiCore.VertexBuffer);
-        _device.Indices = GreyGuiCore.IndexBuffer;
-
-        Matrix projection = Matrix.CreateOrthographicOffCenter(0, _device.Viewport.Width, _device.Viewport.Height, 0, 0, 1);
-
-        _device.BlendState = BlendState.NonPremultiplied;
-        _device.RasterizerState = ScissorState;
-
-        _uiShader.Parameters["WorldViewProjection"].SetValue(projection);
-        foreach (DrawBatch batch in context.Batches)
-        {
-            if (batch.IndexCount == 0) continue;
-
-            _device.SamplerStates[0] = batch.Texture == GreyGuiCore.Atlas ? SamplerState.LinearClamp : SamplerState.PointClamp;
-            _device.ScissorRectangle = batch.Scissor;
-            _uiShader.Parameters["Texture"].SetValue(batch.Texture);
-
-            foreach (EffectPass pass in _uiShader.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                _device.DrawIndexedPrimitives(
-                    PrimitiveType.TriangleList,
-                    0,
-                    batch.IndexOffset,
-                    batch.IndexCount / 3
-                );
-            }
-        }
-        context.Clear();
-    }
-
-
-    /// <summary>
-    /// Collect drawing information from a GUI tree of a given GreyGuiElement root.
-    /// </summary>
-    /// <remarks>
-    /// Like SpriteBatch.Draw, this method does NOT immediately draw to the current render target, instead, it add draw commands into the RenderContext.<br/> Use GuiBatch.Flush() to actually render the pixels to the render target.
-    /// </remarks>
-    /// <param name="root">The GreyGuiElement and its children you want to draw</param>
-    /// <param name="context">Drawing RenderContext</param>
     /// <param name="position">Drawing position</param>
     public void Draw(GreyGuiElement root, Point position)
     {
@@ -144,7 +70,6 @@ public class GuiBatch
     /// <summary>
     /// Consume accumulated draw commands of a RenderContext and render to the screen 
     /// </summary>
-    /// <param name="context"></param>
     public void Flush()
     {
         // Check if there are generated SDF bitmaps that haven't be drawn to the atlas
